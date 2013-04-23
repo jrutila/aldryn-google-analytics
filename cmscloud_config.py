bb680573220da4e147c7efae6b85d365a75b4497
Krzysiek Szularz <szuliq@gmail.com>
2013-04-23 11:26:46 +0200

    Add Google Analytics ID validation

--- cmscloud_config.py
+++ cmscloud_config.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
 from cmscloud_client import forms
+import re
+
+
+class GoogleAnalyticsIDField(forms.CharField):
+
+    GOOGLE_ANALYTICS_ID_FORMAT = re.compile(r'^UA-\d+(-\d+)?$')
+
+    def clean(self, value):
+        value = super(GoogleAnalyticsIDField, self).clean(value.strip())
+        if not self.GOOGLE_ANALYTICS_ID_FORMAT.match(value):
+            raise forms.ValidationError('Invalid format. Google Analytics site tracking ID format is: UA-XXXXXX-YY.')
+        return value
 
 
 class Form(forms.BaseForm):
 
-    google_analytics_id = forms.CharField('Tracking ID', max_length=50)
+    google_analytics_id = GoogleAnalyticsIDField('Tracking ID', max_length=50)
 
     def to_settings(self, data, settings):
         settings['GOOGLE_ANALYTICS_ID'] = data['google_analytics_id']
