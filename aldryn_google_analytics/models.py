5d0de5504a2d606bb5b3907db968dd6a56559227
Stefan Foulis <stefan@foulis.ch>
2014-02-07 23:15:22 +0100

    adds universal analytics and individual user tracking

--- aldryn_google_analytics/models.py
+++ aldryn_google_analytics/models.py
@@ -14,11 +14,34 @@ GOOGLE_ANALYTICS_SCRIPT = """<script type="text/javascript">
 </script>"""
 
 
-def get_google_analytics_script():
-    google_analytics_id = getattr(settings, 'GOOGLE_ANALYTICS_ID', None)
-    if google_analytics_id:
-        return GOOGLE_ANALYTICS_SCRIPT % {'google_analytics_id': google_analytics_id}
-    else:
-        return ''
+UNIVERSAL_ANALYTICS_SCRIPT = """<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-registry.add_to_tail(get_google_analytics_script())
+  ga('create', '%(google_analytics_id)s', 'auto');%(ga_individual)s
+  ga('send', 'pageview');
+
+</script>"""
+
+
+def get_google_analytics_script(request):
+    google_analytics_id = getattr(settings, 'GOOGLE_ANALYTICS_ID', None)
+    use_universal_analytics = getattr(settings, 'GOOGLE_ANALYTICS_USE_UNIVERSAL', False)
+    track_individuals = getattr(settings, 'GOOGLE_ANALYTICS_TRACK_INDIVIDUALS', False)
+    if not google_analytics_id:
+        return ''
+    context = {
+        'google_analytics_id': google_analytics_id,
+        'ga_individual': '',
+    }
+    if track_individuals and not request.user.is_anonymous():
+        context['ga_individual'] = """\n  ga('set', '&uid', %s);'""" % request.user.id
+    if use_universal_analytics:
+        template = UNIVERSAL_ANALYTICS_SCRIPT
+    else:
+        template = GOOGLE_ANALYTICS_SCRIPT
+    return template % context
+
+registry.add_to_tail(get_google_analytics_script)
